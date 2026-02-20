294877694784cbf455344cf01b404a6a39a4cf47
meenakshi930 <meenakshi.gupta_cs.aiml24@gla.ac.in>
2026-02-20 10:22:52 +0530

    add XML configuration files and update main.cpp for RGB LED control

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,15 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Meenakshi
+ * @date 2026-02-17
  *
  * @details
- * Controls RGB LED using digital ON/OFF
+ * Controls an RGB LED using digital ON/OFF
  * and analog PWM brightness control.
+ * Demonstrates digital blinking and PWM color cycling.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
-
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
-
-void setup() {
-
-    // TODO 4:
-    // Initialize Serial communication (9600 baud)
-
-    // TODO 5:
-    // Configure RGB pins as OUTPUT
-
-    // TODO 6:
-    // Print initialization message
-}
-
-void loop() {
-
-    // -------- DIGITAL MODE --------
-
-    // TODO 7:
-    // Turn ON red (digital HIGH)
-
-    // TODO 8:
-    // Turn OFF red
-
-    // -------- ANALOG (PWM) MODE --------
-
-    // TODO 9:
-    // Set RED brightness using analogWrite()
-
-    // TODO 10:
-    // Set GREEN brightness using analogWrite()
-
-    // TODO 11:
-    // Set BLUE brightness using analogWrite()
-
-    // TODO 12:
-    // Add delay for visible transition
-}
+int RED_pin = 9;
+int GREEN_pin = 10;
+int BLUE_pin = 11;
